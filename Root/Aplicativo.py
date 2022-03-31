04c1897b023b4f3420c949e12d85a6b67ae58a5e
Vicente Renart Neto <vinirenart@yahoo.com.br>
2022-03-30 23:25:20 -0300

    Aplicativo para consulta cep, tentativa ainda em construção

---
+++ Root/Aplicativo.py
@@ -0,0 +1,72 @@
+from consulta import *
+from interface import *
+
+
+tela_ini()
+    while True:
+    window, event, values = sg.read_all_windows()
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'DDD':
+        window.close()
+        tela_ddd()
+        while True:
+            window, event, values = sg.read_all_windows()
+
+            if event == sg.WIN_CLOSED:
+                exit()
+
+            elif event == 'Voltar':
+                window.close()
+                tela_ini()
+                break
+
+            elif event == 'Consultar':
+                try:
+                    estado = consulta_ddd(values['ddd'])['state']
+                    cidades = consulta_ddd(values['ddd'])['cities']
+                    window['cidades'].update('')
+                    for k, v in estados.items():
+                        if k == estado:
+                            window['estado'].update(v)
+
+                    for i in consulta_ddd(values['ddd'])['cities']:
+
+                        print(i.title())
+                except:
+                    sg.Popup('Verifique se o campo "DDD" foi preenchido corretamente\n'
+                             '                       ou se está conectado a internet', font='arial 12', title='ERRO')
+
+    elif event == 'CEP':
+        window.close()
+        tela_cep()
+        while True:
+            window, event, values = sg.read_all_windows()
+
+            if event == sg.WIN_CLOSED:
+                exit()
+
+            elif event == 'Consultar':
+                try:
+                    logradouro = consulta_cep(values['cep'])['logradouro']
+                    bairro = consulta_cep(values['cep'])['bairro']
+                    localidade = consulta_cep(values['cep'])['localidade']
+                    uf = consulta_cep(values['cep'])['uf']
+                    ibge = consulta_cep(values['cep'])['ibge']
+                    ddd = consulta_cep(values['cep'])['ddd']
+
+                    window['logradouro'].update(logradouro)
+                    window['bairro'].update(bairro)
+                    window['localidade'].update(localidade)
+                    window['uf'].update(uf)
+                    window['ibge'].update(ibge)
+                    window['ddd'].update(ddd)
+                except:
+                    sg.Popup('Verifique se o campo "CEP" foi preenchido corretamente\n'
+                             '                       ou se está conectado a internet', font='arial 12', title='ERRO')
+
+            elif event == 'Voltar':
+                window.close()
+                tela_ini()
+                break
